b309da16b4596aaa25b34182a5ba2ac740dc865d
mesut <mstrby20@gmail.com>
2021-08-24 20:28:49 +0200

    Initial commit

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,23 @@
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class ZippoTest {
+
+
+    @Test
+    public void test(){
+        given()
+                //hazirlik islemlerini yapacagiz
+                .when()
+                // link ve aksiyon islemleri
+
+                .then()
+                //test ve extract islemleri
+
+
+        ;
+    }
+}
